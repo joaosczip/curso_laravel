df0ef3791ba0b6e8b81c6eb7815a60ce97c285c9
João Guilherme <joao.sczip@hotmail.com>
2018-10-06 12:38:41 -0300

    Subindo o projeto

---
+++ app/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'amount',
+        'price'
+    ];
+
+    protected $table = 'products';
+}
